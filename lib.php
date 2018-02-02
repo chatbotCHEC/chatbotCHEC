1aa02e262cb7314b11d84a983ec25863c7db3403
Juan Camilo <prjchec.jcardona@umanizales.edu.co>
2018-02-02 17:35:39 -0500

    ready for test deploy6

--- lib.php
+++ lib.php
@@ -222,7 +222,7 @@ class chatBotAPI {
     public function getIndisNiu($niu){
         $json['speech'] = $this->getIndisponibilidad($niu);
         $json['displayText'] = $this->getIndisponibilidad($niu);
-        $json['messages'] = array(array('speech' => $this->getIndisponibilidad($niu), 'platform'=>'telegram')); 
+        $json['messages'] = array(array('platform'=>'telegram', 'speech' => $this->getIndisponibilidad($niu))); 
         return $json;
     }
 
@@ -230,7 +230,7 @@ class chatBotAPI {
     public function getSPNiu($niu){
         $json['speech'] = $this->getSuspensionesProgramadas($niu);
         $json['displayText'] = $this->getSuspensionesProgramadas($niu);
-        $json['messages'] = array(array('speech' => $this->getSuspensionesProgramadas($niu), 'platform'=>'telegram')); 
+        $json['messages'] = array(array('platform'=>'telegram', 'speech' => $this->getSuspensionesProgramadas($niu))); 
         return $json;
     }
 
