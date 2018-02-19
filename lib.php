1ca6d3907081c2d3e57fab8e18b671df6ba08b56
daniel2389 <prjchec.dcardona@umanizales.edu.co>
2018-02-19 16:17:44 -0500

    backup chatbot7

--- lib.php
+++ lib.php
@@ -238,13 +238,13 @@ class chatBotAPI {
                                 array(                                
                                     array(
                                         'text' => 'Si',
-                                        'callback_data' => 'Si ✔'
+                                        'callback_data' => 'Si' '\U00002714'
                                         )
                                     ),
                                 array(                                
                                     array(
                                         'text' => 'No',
-                                        'callback_data' => 'No ✖'
+                                        'callback_data' => 'No''\U0000274C'
                                         )
                                     )
                             ),
