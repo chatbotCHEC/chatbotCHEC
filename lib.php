d26997d6b2d51f76c04e056810931e907c793fef
Juan Camilo <prjchec.jcardona@umanizales.edu.co>
2018-02-19 11:22:59 -0500

    prueba boton desde back2

--- lib.php
+++ lib.php
@@ -225,22 +225,24 @@ class chatBotAPI {
     public function getIndisNiu($niu){
         $json['speech'] = $this->getIndisponibilidad($niu);
         $json['displayText'] = $this->getIndisponibilidad($niu);
-        $json['data'] = array(
-            'telegram' => array(
-                'text' => $this->getIndisponibilidad($niu)."\n ¿Deseas consultar algo más?",
-                'reply_markup' => array(
-                    'inline_keyboard' => array(
-                        array(
-                            'text' => 'Si', 
-                            'callback_data' => 'Si'
-                        ),
-                        array(
-                            'text' => 'No',
-                            'callback_data' => 'No'
-                        )
-                    ), 
-                )
-            ), 
+        $json['messages'] = array(
+            'data' => array(
+                'telegram' => array(
+                    'text' => $this->getIndisponibilidad($niu)."\n ¿Deseas consultar algo más?",
+                    'reply_markup' => array(
+                        'inline_keyboard' => array(
+                            array(
+                                'text' => 'Si', 
+                                'callback_data' => 'Si'
+                            ),
+                            array(
+                                'text' => 'No',
+                                'callback_data' => 'No'
+                            )
+                        ), 
+                    )
+                ), 
+            )
         );
         return $json;
     }
