25c3df63a510035e8ebcfb87969cc227c8f81d18
daniel2389 <prjchec.dcardona@umanizales.edu.co>
2018-03-08 08:46:26 -0500

    actualizacion 4

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+
+// El acceso a caracteres específicos en una cadena 
+// se puede conseguir usando "corchetes"
+$string = '3022_8-rx realizada';
+echo $string[5];                 // 8
+
+
+$string2 = '2622_3-suspension realizada';
+echo $string2[5];               // 3
+
+?>
