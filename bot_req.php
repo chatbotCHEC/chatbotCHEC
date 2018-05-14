433fa444409807617dd5b5a2c753191b916313ab
Juan Camilo Cardona <prjchec.jcardona@umanizales.edu.co>
2018-05-14 18:19:05 -0500

    calificacion ready5

--- bot_req.php
+++ bot_req.php
@@ -107,7 +107,7 @@ foreach ($contexts as $i => $con) {
             $answered = true;
             break;
         case 'calificacion':
-            $calificacion['calificacion'] = substr($reqBody['result']['resolvedQuery'],4);
+            $calificacion['calificacion'] = substr($reqBody['result']['resolvedQuery'],5);
             $calificacion['id'] = $reqBody['id'];
             $calificacion['sessionId'] = $reqBody['sessionId'];
             $calificacion['date'] = $reqBody['timestamp'];
